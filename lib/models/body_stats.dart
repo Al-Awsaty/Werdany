6d1a7d928b5e32a64c4c0dbfea01fde5cb8915db
Sakura <amir@alawsaty.com>
2025-01-09 08:37:31 +0300

    Enhance UI with tooltips, zooming, and panning (#3)

--- lib/models/body_stats.dart
+++ lib/models/body_stats.dart
@@ -2,14 +2,16 @@ class BodyStats {
   double weight;
   double muscleMass;
   double fatPercentage;
+  DateTime date;
 
-  BodyStats({required this.weight, required this.muscleMass, required this.fatPercentage});
+  BodyStats({required this.weight, required this.muscleMass, required this.fatPercentage, required this.date});
 
   factory BodyStats.fromJson(Map<String, dynamic> json) {
     return BodyStats(
       weight: json['weight'],
       muscleMass: json['muscleMass'],
       fatPercentage: json['fatPercentage'],
+      date: DateTime.parse(json['date']),
     );
   }
 
@@ -18,6 +20,7 @@ class BodyStats {
       'weight': weight,
       'muscleMass': muscleMass,
       'fatPercentage': fatPercentage,
+      'date': date.toIso8601String(),
     };
   }
 }
--- lib/widgets/body_stat_chart.dart
+++ lib/widgets/body_stat_chart.dart
@@ -23,6 +23,19 @@ class BodyStatChart extends StatelessWidget {
             colors: [Colors.blue],
             barWidth: 4,
             belowBarData: BarAreaData(show: false),
+            dotData: FlDotData(show: true),
+            showingIndicators: [0, 1, 2, 3, 4, 5],
+            tooltipData: FlTooltipData(
+              tooltipBgColor: Colors.blueAccent,
+              getTooltipItems: (List<LineBarSpot> touchedSpots) {
+                return touchedSpots.map((barSpot) {
+                  return LineTooltipItem(
+                    '${barSpot.y}',
+                    const TextStyle(color: Colors.white),
+                  );
+                }).toList();
+              },
+            ),
           ),
           LineChartBarData(
             spots: bodyStats
@@ -35,6 +48,19 @@ class BodyStatChart extends StatelessWidget {
             colors: [Colors.green],
             barWidth: 4,
             belowBarData: BarAreaData(show: false),
+            dotData: FlDotData(show: true),
+            showingIndicators: [0, 1, 2, 3, 4, 5],
+            tooltipData: FlTooltipData(
+              tooltipBgColor: Colors.greenAccent,
+              getTooltipItems: (List<LineBarSpot> touchedSpots) {
+                return touchedSpots.map((barSpot) {
+                  return LineTooltipItem(
+                    '${barSpot.y}',
+                    const TextStyle(color: Colors.white),
+                  );
+                }).toList();
+              },
+            ),
           ),
           LineChartBarData(
             spots: bodyStats
@@ -47,6 +73,19 @@ class BodyStatChart extends StatelessWidget {
             colors: [Colors.red],
             barWidth: 4,
             belowBarData: BarAreaData(show: false),
+            dotData: FlDotData(show: true),
+            showingIndicators: [0, 1, 2, 3, 4, 5],
+            tooltipData: FlTooltipData(
+              tooltipBgColor: Colors.redAccent,
+              getTooltipItems: (List<LineBarSpot> touchedSpots) {
+                return touchedSpots.map((barSpot) {
+                  return LineTooltipItem(
+                    '${barSpot.y}',
+                    const TextStyle(color: Colors.white),
+                  );
+                }).toList();
+              },
+            ),
           ),
         ],
         titlesData: FlTitlesData(
@@ -55,6 +94,18 @@ class BodyStatChart extends StatelessWidget {
         ),
         borderData: FlBorderData(show: true),
         gridData: FlGridData(show: true),
+        lineTouchData: LineTouchData(
+          touchTooltipData: LineTouchTooltipData(
+            tooltipBgColor: Colors.blueGrey,
+          ),
+          touchCallback: (LineTouchResponse touchResponse) {},
+          handleBuiltInTouches: true,
+        ),
+        panAndZoomData: PanAndZoomData(
+          enabled: true,
+          maxScale: 2,
+          minScale: 0.5,
+        ),
       ),
     );
   }
